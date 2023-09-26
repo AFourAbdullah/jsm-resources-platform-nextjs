09f2549b519d1e1aabc258ffc8d96184baec8385
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-26 16:49:42 +0500

    files added

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1>HOme</h1>
+      <h1>HOme</h1>
+      <h1>HOme</h1>
+      <h1>HOme</h1>
+      <h1>HOme</h1>
+      <h1>HOme</h1>
+      <h1>HOme</h1>
+      <h1>HOme</h1>
+    </div>
+  );
 };
 
 export default page;
